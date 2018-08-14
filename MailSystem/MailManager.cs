96e6fbd3b2608463d3cb5fafa7d620a1a4551bf3
Iosi Ginerman <iginerman@hotmail.com>
2018-08-14 15:07:40 +0300

    3rd class

---
+++ MailSystem/MailManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSystem
+{
+    public class MailArrivedEventArgs : EventArgs
+    {
+        public readonly string _title;
+        public readonly string _body;
+
+        public MailArrivedEventArgs()
+        {
+            _title = "Dummy title";
+            _body = "Dummy body";
+        }
+
+        public MailArrivedEventArgs(string title, string body)
+        {
+            _title = title;
+            _body = body;
+        }
+    }
+
+    public class MailManager
+    {
+        public event EventHandler<MailArrivedEventArgs> MailArrived;
+
+        public MailManager()
+        {
+
+        }
+
+        protected virtual void OnMailArrived(MailArrivedEventArgs e)
+        {
+            if (MailArrived != null)
+            {
+                MailArrived(this, e);
+            }
+        }
+
+        public void SimulateMailArrived()
+        {
+            OnMailArrived(new MailArrivedEventArgs());
+        }
+
+        public void PrintTwice(object sender, MailArrivedEventArgs e)
+        {
+            string all = e._title + " " + e._body;
+            Console.WriteLine("*" + all + "*");
+        }
+    }
+}
---
+++ ThreadedPrimeCalc/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadedPrimeCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int primes, min;
+            Stopwatch s = new Stopwatch();
+            Dictionary<long, int> ThreadsToTimeDict = new Dictionary<long, int>();
+
+            for (int i = 2; i < 15; i++)
+            {
+                s.Start();
+                primes = CalcPrimes(3, 10000000, i);
+                s.Stop();
+                Console.WriteLine("With {0} threads is took {1}ms to find the {2} primes", i, s.ElapsedMilliseconds, primes);
+                ThreadsToTimeDict[s.ElapsedMilliseconds] = i;
+                s.Reset();
+            }
+            long bestTime = ThreadsToTimeDict.Keys.Min();
+            Console.WriteLine("The best time is {0} with {1} threads", bestTime, ThreadsToTimeDict[bestTime]);
+            Console.ReadKey();
+        }
+
+        public static int CalcPrimes(int from, int to, int numOfThreads)
+        {
+            ConcurrentQueue<int> primes = new ConcurrentQueue<int>();
+            List<Thread> threads = new List<Thread>();
+            int delta = (to - from) / numOfThreads;
+            for (int i = 0; i < numOfThreads; i++)
+            {
+                int start = from + (delta * i), end = from + (delta * (i + 1));
+                if (i == numOfThreads - 1) end = to;
+
+                Thread t = new Thread(() => 
+                {
+                    int counter = 0;
+                    for (int num = start; num < end; num++)
+                    {
+                        if (IsPrime(num))
+                        {
+                            counter++;
+                            primes.Enqueue(num);
+                        }
+                    }
+                    //Console.WriteLine("From {0} to {1} there are: {2}", start, end, counter);
+                });
+                threads.Add(t);
+            }
+
+            foreach (Thread t in threads)
+                t.Start();
+
+            foreach (Thread t in threads)
+                t.Join();
+
+            return primes.Count();
+
+        }
+
+        public static bool IsPrime(int num)
+        {
+            if (num <= 1) return false;
+            if (num < 4) return true;
+            for (int i = 2; i <= Math.Sqrt(num); i++)
+                if (num % i == 0)
+                    return false;
+            return true;
+        }
+
+
+    }
+}
