420537c4668bd6fa7c35a9a78103cabd259cae53
Eon Torch (Lenovo S310) <idanl22@gmail.com>
2018-08-14 15:35:00 +0300

    Brought back the "cooler" IsPrime

--- ThreadedPrimeCalcIDAN/Program.cs
+++ ThreadedPrimeCalcIDAN/Program.cs
@@ -57,14 +57,12 @@ namespace ThreadedPrimeCalcIDAN
         private static bool IsPrime(int number)
         {
             if (number < 2) return false;
-
-            int root = (int) Math.Sqrt((double) number);
-
-            for (int i = 2; i <= root; i++)
+            if (number % 2 == 0) return (number == 2);
+            int root = (int)Math.Sqrt((double)number);
+            for (int i = 3; i <= root; i += 2)
             {
                 if (number % i == 0) return false;
             }
-
             return true;
         }
     }
