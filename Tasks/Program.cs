d39d70116983156058a5584c126cf2977eaa1fc1
Eon Torch (Lenovo S310) <idanl22@gmail.com>
2018-08-21 12:49:13 +0300

    Added task cancellation example (from the pdf, page 179)

---
+++ Tasks/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Tasks
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            RunDemo();
+        }
+        
+        public static void RunDemo() {
+            var cts = new CancellationTokenSource();
+            Task t = Task.Factory.StartNew(() => DoWork(cts.Token, 100));
+            Console.WriteLine("Press ENTER to cancel operation");
+            Console.ReadLine();
+            cts.Cancel();
+            try {
+                t.Wait();
+                Console.WriteLine("Task done.");
+            }
+            catch(AggregateException ex) {
+                ex.Handle(e => e is OperationCanceledException);
+                Console.WriteLine("Task cancelled.");
+            }
+        }
+        private static void DoWork(CancellationToken ct, int data) {
+            for(int i = 0; i < data; i++) {
+                ct.ThrowIfCancellationRequested();
+                // do some work...
+                Console.WriteLine(i);
+                Thread.Sleep(50); // waste some time
+            }
+        }
+    }
+}
