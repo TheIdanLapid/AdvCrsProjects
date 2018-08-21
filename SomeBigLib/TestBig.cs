c58ab81a37220ff95a4da896b12bc509befbaac2
Eon Torch (Lenovo S310) <idanl22@gmail.com>
2018-08-21 11:09:29 +0300

    ThreadPool management with AutoResetEvent example

---
+++ SomeBigLib/TestBig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace SomeBigLib
+{
+    public class TestBig
+    {
+        private static string Hello(string str)
+        {
+            return "";
+        }
+
+        static void Main(string[] args)
+        {
+            var ev = new AutoResetEvent(false);
+            ThreadPool.QueueUserWorkItem(data =>
+            {
+                Console.WriteLine($"ThreadPool thread {Thread.CurrentThread.ManagedThreadId}.");
+                Thread.Sleep(3000);
+                Console.WriteLine("ThreadPool thread done.");
+                ev.Set();
+            }, 7);
+            Console.WriteLine("Main thread working...");
+            ev.WaitOne(); // AutoResetEvent is live and false, waiting for it to become "true"
+            Console.WriteLine("ALL DONE!");
+        }
+    }
+}
---
+++ ThreadPoolAndAutoResetEvent/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace ThreadPoolAndAutoResetEvent
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
