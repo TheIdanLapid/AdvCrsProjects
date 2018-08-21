2569f56b1952f37e3d1242b8d8e2bb76f0e31f19
Eon Torch (Lenovo S310) <idanl22@gmail.com>
2018-08-21 11:17:03 +0300

    Moved "Main" to new console proj.
    Didn't run on Class Lib proj.

--- SomeBigLib/TestBig.cs
+++ SomeBigLib/TestBig.cs
@@ -10,19 +10,6 @@ namespace SomeBigLib
             return "";
         }
 
-        static void Main(string[] args)
-        {
-            var ev = new AutoResetEvent(false);
-            ThreadPool.QueueUserWorkItem(data =>
-            {
-                Console.WriteLine($"ThreadPool thread {Thread.CurrentThread.ManagedThreadId}.");
-                Thread.Sleep(3000);
-                Console.WriteLine("ThreadPool thread done.");
-                ev.Set();
-            }, 7);
-            Console.WriteLine("Main thread working...");
-            ev.WaitOne(); // AutoResetEvent is live and false, waiting for it to become "true"
-            Console.WriteLine("ALL DONE!");
-        }
+       
     }
 }
--- ThreadPoolAndAutoResetEvent/Program.cs
+++ ThreadPoolAndAutoResetEvent/Program.cs
@@ -1,9 +1,23 @@
-﻿namespace ThreadPoolAndAutoResetEvent
+﻿using System;
+using System.Threading;
+
+namespace ThreadPoolAndAutoResetEvent
 {
     internal class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
+            var ev = new AutoResetEvent(false);
+            ThreadPool.QueueUserWorkItem(data =>
+            {
+                Console.WriteLine($"ThreadPool thread {Thread.CurrentThread.ManagedThreadId}.");
+                Thread.Sleep(3000);
+                Console.WriteLine("ThreadPool thread done.");
+                ev.Set();
+            }, 7);
+            Console.WriteLine("Main thread working...");
+            ev.WaitOne(); // AutoResetEvent is live and false, waiting for it to become "true"
+            Console.WriteLine("ALL DONE!");
         }
     }
 }
